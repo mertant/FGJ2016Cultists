434de5aee39712517962c5b04f25bdd80d44e3cb
Henri Niva <henri.niva@aalto.fi>
2016-01-29 23:37:56 +0200

    World.js- koodia

--- src/js/Objects/world.js
+++ src/js/Objects/world.js
@@ -13,14 +13,41 @@ function Map() {
             this.data[i][j] = null;
         }
     }*/
+
+    //group for player-collideable objects - walls, boulder
+    this.collideableGroup = game.add.group();
+    //group for other objects that spawn in the map - resources, bunny
+    this.noncollideableGroup = game.add.group();
 }
 
 Map.prototype.add = function(x, y, obj) {
     //this.data[x][y] = obj;
     this.objects.push(obj);
+
+    // add object to proper collision group
 }
 
-Map.prototype.getAt = function(x, y, obj) {
-    for (var i=0; i<objects.length; i++){
+// Removes an object from the map objects
+// Returns if the operation was succesfull
+Map.prototype.remove = function(obj) {
+    var index = this.objects.indexOf(obj);
+    if (index === -1) {
+        return false;
     }
+
+    this.objects.splice(index, 1);
+    return true;
+}
+
+// Returns an object in the map at the given SCREEN X/Y coordinates
+// Returns null if no object exists at x, y
+Map.prototype.getAt = function(x, y) {
+    for (var i=0; i<objects.length; i++){
+        var obj = this.objects[i];
+        if (x > obj.sprite.x && x < obj.sprite.x + obj.sprite.width &&
+            y > obj.sprite.y && y < obj.sprite.y + obj.sprite.height) {
+            return obj;
+        }
+    }
+    return null;
 }

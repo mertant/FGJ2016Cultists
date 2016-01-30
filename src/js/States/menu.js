207936c5a2072e09b7c05715ec7ed11e99f3a14f
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-01-30 18:51:56 +0200

    Add some comments.

--- src/js/States/menu.js
+++ src/js/States/menu.js
@@ -11,7 +11,7 @@ Menu.prototype = {
     }, 
 
     create: function() {
-        // Create menu objects
+        // Goat head sprite
         this.goathead = game.add.sprite(game.world.centerX, 0.5 * game.world.centerY, "goaty");
         this.goathead.anchor.set(0.5);
         this.goathead.scale.setTo(0.5);
@@ -19,19 +19,19 @@ Menu.prototype = {
         this.goatheadTween = game.add.tween(this.goathead);
         this.goatheadTween.to({angle: 5}, 2000, Phaser.Easing.Sinusoidal.InOut, true, delay = 0, repeat = -1, yoyo = true);
 
+        // Enter key sprite
         this.keySprite = game.add.sprite(game.world.centerX, 1.5 * game.world.centerY, "enter-key");
         this.keySprite.anchor.set(0.5);
         this.keySprite.scale.setTo(3.0);
         this.keySprite.smoothed = false;
-
         this.keySpriteTween = game.add.tween(this.keySprite);
         this.keySpriteTween.to({y: '+20'}, 800, Phaser.Easing.Sinusoidal.InOut, true, delay = 0, repeat = -1, yoyo = true);
 
-        // Pressing the enter key goes to game
         this.enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
     },
 
     update: function() {
+        // Pressing the enter key starts the game
         if (this.enterKey.isDown) {
             game.state.start("Game");
         }

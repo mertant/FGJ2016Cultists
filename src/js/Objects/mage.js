d30f1c8188bbb311d03c664ac1888948c39b981d
Henri Niva <henri.niva@aalto.fi>
2016-01-30 12:54:11 +0200

    Mage item management

--- src/js/Objects/mage.js
+++ src/js/Objects/mage.js
@@ -1,6 +1,7 @@
 function Mage(x, y, spritekey) {
     this.sprite = game.add.sprite(x, y, spritekey);
     this.inventory = [null, null, null];
+    this.weapon = null;
 
     //Animations
     this.standAnim = this.sprite.animations.add("stand", [0]);
@@ -30,6 +31,38 @@ Mage.prototype.move = function() {
 
 Mage.prototype.stand = function() {
     if (this.sprite.animations.currentAnim != this.standAnim) {
-        this.standAnim.play(1, true);
+        this.standAnim.play(10, true);
     }
+}
+
+Mage.prototype.pickUp = function(resource) {
+    //picks up the given resource to the inventory, if possible
+    //returns true if the operation was succesfull, false otherwise
+    for (var i = 0; i < this.inventory.length; i++) {
+        if (this.inventory[i] == null) {
+            this.inventory[i] = resource;
+            return true;
+        }
+    }
+    return false;
+}
+
+Mage.prototype.dumpItems = function() {
+    // Makes the mage drop all of their items, and returns them in an array
+    var temparray = [];
+    for (var i = 0; i < this.inventory.length; i++) {
+        if (this.inventory[i] != null) {
+            temparray.push(this.inventory[i]);
+            this.inventory[i] = null;
+        }
+    }
+    return temparray;
+}
+
+Mage.prototype.useWeapon = function() {
+    // Uses the weapon in the current slot and returns it.
+    // Returns null if there is no weapon
+    var temp = this.weapon;
+    this.weapon = null;
+    return temp;
 }
